cba40f21b61ed7492a0e0dc4c06e75316b162f24
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-25 15:53:42 +0100

    fatto db

---
+++ database.php
@@ -0,0 +1,66 @@
+<?php
+
+$database = [
+    [
+      'title' => 'New Jersey',
+      'author' => 'Bon Jovi',
+      'year' => 1988,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+    [
+      'title' => 'Meet The Residents',
+      'author' => 'The Residents',
+      'year' => 1974,
+      'poster' => 'https://www.discogs.com/The-Residents-Meet-The-Residents/master/20938#images/1888526'
+    ],
+    [
+      'title' => 'Alberto Fortis',
+      'author' => 'Alberto Fortis',
+      'year' => 1979,
+      'poster' => 'https://www.discogs.com/Alberto-Fortis-Alberto-Fortis/release/1244826#images/4769505'
+    ],
+    [
+      'title' => 'SxM',
+      'author' => 'Sangue Misto',
+      'year' => 1994,
+      'poster' => 'https://www.discogs.com/Sangue-Misto-SXM/master/297932#images/9060839'
+    ],
+    [
+      'title' => 'Turbe Giovanili',
+      'author' => 'Fabri Fibra',
+      'year' => 2002,
+      'poster' => 'https://www.discogs.com/Fabri-Fibra-Turbe-Giovanili/master/469093#images/3407709'
+    ],
+    [
+      'title' => 'Storia Di Un Impiegato',
+      'author' => 'Fabrizio De André',
+      'year' => 1973,
+      'poster' => 'https://www.discogs.com/Fabrizio-De-Andr%C3%A9-Storia-Di-Un-Impiegato/release/1851110#images/42860842'
+    ],
+    [
+      'title' => 'The Dark Side Of The Moon',
+      'author' => 'Pink Floyd',
+      'year' => 1973,
+      'poster' => 'https://www.discogs.com/Pink-Floyd-The-Dark-Side-Of-The-Moon/master/10362#images/24290820'
+    ],
+    [
+      'title' => 'La Morte Dei Miracoli',
+      'author' => 'Frankie Hi-NRG MC',
+      'year' => 1997,
+      'poster' => 'https://www.discogs.com/Frankie-Hi-NRG-MC-La-Morte-Dei-Miracoli/master/328989#images/37514126'
+    ],
+    [
+      'title' => 'I Borghesi',
+      'author' => 'Giorgio Gaber',
+      'year' => 1997,
+      'poster' => 'https://www.discogs.com/Giorgio-Gaber-I-Borghesi/master/745352#images/34324023'
+    ],
+    [
+      'title' => 'Friends On Mushrooms',
+      'author' => 'Infected Mushroom',
+      'year' => 2015,
+      'poster' => 'https://www.discogs.com/Infected-Mushroom-Friends-On-Mushrooms/master/780501#images/19827399'
+    ],
+
+]
+?>
