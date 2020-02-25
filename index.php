f7b4eb3d2da3a9d98f7cc30bc77ca046186ff1b6
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-25 16:09:41 +0100

    up

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ 'database.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial scale= 1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Php Dischi</title>
+  </head>
+  <body>
+  <script src="dist/app.js"></script>
+  </body>
+</html>
