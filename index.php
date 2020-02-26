af8ddf7fb2f62f2f99c5e0bd97ea1ae5341bbae7
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-26 09:20:55 +0100

    up

--- index.php
+++ index.php
@@ -21,13 +21,13 @@ include __DIR__ . '/database.php';
       </div>
     </header>
     <main>
-      <div class="test flex">
+      <div class="main_container flex">
         <?php foreach ($database as $disco) { ?>
           <div class="disk_container">
             <img class="poster" src="<?= $disco['poster'] ?>" alt="copertina">
-           <h2>Title: <?php echo $disco['title'] ?></h2>
-           <p>Author: <?php echo $disco['author'] ?></p>
-           <p>Year: <?php echo $disco['year'] ?></p>
+           <h2><?php echo $disco['title'] ?></h2>
+           <p><?php echo $disco['author'] ?></p>
+           <p><?php echo $disco['year'] ?></p>
          </div>
        <?php } ?>
       </div>
